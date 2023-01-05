916e7943c8b4eaf5b086aaa929d1e259b394c1ae
olesien <olesien2@gmail.com>
2023-01-06 00:56:16 +0100

    Fail

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -3,8 +3,10 @@ import styles from "../styles/Header.module.scss";
 
 export default function Basket({ toggleCart }: { toggleCart: () => void }) {
     return (
-        <div className={styles.cartContainer}>
-            <div className={styles.cart}>Cart</div>
+        <div>
+            <div className={styles.cartContainer}>
+                <div className={styles.cart}>Cart</div>
+            </div>
             <div className="overlay" onClick={toggleCart}></div>
         </div>
     );
