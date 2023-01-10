df94a01f3b61e46d255ca2415d67502490bc5d7e
olesien <olesien2@gmail.com>
2023-01-10 13:34:24 +0100

    Added confetti!

--- pages/successfulorder.tsx
+++ pages/successfulorder.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Layout from "../components/layout";
 import mainStyles from "../styles/Main.module.scss";
+import utilStyles from "../styles/utils.module.scss";
+import Confetti from "react-confetti";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 export default function successfulorder() {
+    const { width, height } = useWindowDimensions();
     return (
         <Layout nonav={true} title="Ordrar - Beställt!">
             <div className={mainStyles.main}>
-                <p>Order beställd!</p>
+                <Confetti width={width} height={height} />
+                <div className={utilStyles.successfulorder}>
+                    <div>
+                        <p>Order beställd!</p>
+                    </div>
+                </div>
             </div>
         </Layout>
     );
