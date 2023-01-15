0e239265b93c2fdf154fed28efb5976c2e17ee66
olesien <olesien2@gmail.com>
2023-01-15 15:33:50 +0100

    Added ability to add cat

--- pages/admin/addproduct.tsx
+++ pages/admin/addproduct.tsx
@@ -3,16 +3,27 @@ import AdminRoute from "../../components/generic/AdminRoute";
 import Layout from "../../components/layout";
 import MainAccount from "../../components/MainAccount";
 import utilStyles from "../../styles/utils.module.scss";
-import styles from "../../styles/Account.module.scss";
+import adminStyles from "../../styles/Admin.module.scss";
 import useQueries from "../../hooks/useQueries";
 import Button from "@mui/material/Button";
-import useUser from "../../lib/useUser";
 import FormInput from "../../components/generic/FormInput";
 import { categories } from "@prisma/client";
 import { getAllCategories } from "../../lib/category";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import translate from "../../lib/translations";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faPlusCircle,
+    faTrash,
+    faUpload,
+} from "@fortawesome/free-solid-svg-icons";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
 
 export interface Product {
     name?: string;
@@ -23,22 +34,67 @@ export interface Product {
     oldprice?: string;
     instock?: string;
 }
+function generate(element: React.ReactElement) {
+    return [0, 1, 2].map((value) =>
+        React.cloneElement(element, {
+            key: value,
+        })
+    );
+}
 
 export default function productlist({
     categories,
 }: {
     categories: Pick<categories, "id" | "name">[];
 }) {
-    const { query, changeQuery } = useQueries();
     const [showNav, setShowNav] = useState(false);
     const [errorMsg, setErrorMsg] = useState("");
     const [errors, setErrors] = useState<Product>({});
     const [form, setForm] = useState<Product>({});
+    const [photos, setPhotos] = useState<File[]>([]);
+    const [newCategory, setNewCategory] = useState<string>("");
+    const [specs, setSpecs] = useState<
+        { name: string; items: { title: string; content: string }[] }[]
+    >([{ name: "", items: [] }]);
+
+    const selectFiles = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { files } = event.target;
+        const selectedFiles = files as FileList;
+        const fileArray = Array.from(selectedFiles);
+        setPhotos((currentPhotos) => {
+            fileArray.forEach((file) => {
+                if (!currentPhotos.find((photo) => photo.name === file.name)) {
+                    //not exists
+                    currentPhotos.push(file);
+                }
+            });
+            //Make unique and update photos
+            return [...currentPhotos];
+        });
+    };
+
+    const addCategory = () => {
+        setSpecs((currentSpecs) => {
+            const exists = currentSpecs.find(
+                (spec) => spec.name === newCategory
+            );
+            if (exists) {
+                return currentSpecs;
+            } else {
+                currentSpecs.push({ name: newCategory, items: [] });
+                return [...currentSpecs];
+            }
+        });
+
+        //Reset cat
+        setNewCategory("");
+    };
 
     const addProduct = () => {
         console.log(form);
     };
     console.log(categories);
+    console.log(photos);
 
     return (
         <AdminRoute>
@@ -47,9 +103,7 @@ export default function productlist({
                 title="Ordrar - Techstore"
             >
                 <MainAccount showNav={showNav}>
-                    <section
-                        className={utilStyles.section + " " + styles.orders}
-                    >
+                    <section className={utilStyles.section}>
                         <p>Lägg till produkt</p>
                         {/* - > Content
                         Product Name
@@ -208,6 +262,130 @@ export default function productlist({
                                             }))
                                         }
                                     />
+                                </div>
+                                <div className={adminStyles.uploadSection}>
+                                    <div>
+                                        <List>
+                                            {photos.map((photo, index) => (
+                                                <ListItem
+                                                    key={index}
+                                                    secondaryAction={
+                                                        <IconButton
+                                                            edge="end"
+                                                            aria-label="delete"
+                                                            onClick={() => {
+                                                                setPhotos(
+                                                                    (
+                                                                        currentPhotos
+                                                                    ) =>
+                                                                        currentPhotos.filter(
+                                                                            (
+                                                                                current
+                                                                            ) =>
+                                                                                current.name !==
+                                                                                photo.name
+                                                                        )
+                                                                );
+                                                            }}
+                                                        >
+                                                            <FontAwesomeIcon
+                                                                icon={faTrash}
+                                                            />
+                                                        </IconButton>
+                                                    }
+                                                >
+                                                    <ListItemAvatar>
+                                                        <Avatar>
+                                                            <img
+                                                                src={URL.createObjectURL(
+                                                                    photo
+                                                                )}
+                                                                alt="X"
+                                                            />
+                                                        </Avatar>
+                                                    </ListItemAvatar>
+                                                    <ListItemText
+                                                        primary={`${photo.name} (${photo.size})`}
+                                                    />
+                                                </ListItem>
+                                            ))}
+                                        </List>
+                                    </div>
+                                    <div className={adminStyles.formSection}>
+                                        <p>Produkt Bilder</p>
+                                        <Button
+                                            variant="contained"
+                                            component="label"
+                                            size="large"
+                                        >
+                                            <FontAwesomeIcon icon={faUpload} />
+                                            Ladda{" "}
+                                            {photos.length > 0
+                                                ? "upp fler"
+                                                : "upp"}{" "}
+                                            bilder
+                                            <input
+                                                // onChange={(e) =>
+                                                //     setActivePhoto(
+                                                //         e.target.files
+                                                //     )
+                                                // }
+                                                onChange={selectFiles}
+                                                type="file"
+                                                hidden
+                                                multiple={true}
+                                            />
+                                        </Button>
+                                    </div>
+                                    {/* SPECS */}
+                                    <div className={adminStyles.formSection}>
+                                        <p>Produkt Specifikationer</p>
+                                        {specs.map((category) => {
+                                            return <div>{category.name}</div>;
+                                        })}
+                                        <div
+                                            className={
+                                                adminStyles.addNewCategory
+                                            }
+                                        >
+                                            <div
+                                                className={
+                                                    adminStyles.categoryInput
+                                                }
+                                            >
+                                                <FormInput
+                                                    type="text"
+                                                    hint=""
+                                                    id={
+                                                        "techstore-category-title"
+                                                    }
+                                                    title={"Titel"}
+                                                    aria={
+                                                        "enter-productspec-title"
+                                                    }
+                                                    error={undefined}
+                                                    value={newCategory}
+                                                    onChange={(newCategory) =>
+                                                        setNewCategory(
+                                                            newCategory
+                                                        )
+                                                    }
+                                                />
+                                            </div>
+                                            <div>
+                                                <Button
+                                                    variant="contained"
+                                                    component="label"
+                                                    onClick={addCategory}
+                                                >
+                                                    <FontAwesomeIcon
+                                                        icon={faPlusCircle}
+                                                    />
+                                                    Lägg till kategori
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div>
                                     <Button type="submit" variant="outlined">
