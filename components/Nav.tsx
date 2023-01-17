e4902f0ecfdd1d86cc29aeb52305ac3116c0c3ce
olesien <olesien2@gmail.com>
2023-01-17 18:18:22 +0100

    Added button select for computer builder

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,8 @@
 import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import NavItem from "./generic/NavItem";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
+import { useState } from "react";
 
 export default function Nav({
     showNav,
@@ -15,9 +18,27 @@ export default function Nav({
         | { header: string }
     )[];
 }) {
+    const [alignment, setAlignment] = useState("standard");
+    const handleChange = (
+        event: React.MouseEvent<HTMLElement>,
+        newAlignment: string
+    ) => {
+        setAlignment(newAlignment);
+    };
     console.log(showNav);
     return (
         <span>
+            <ToggleButtonGroup
+                color="primary"
+                value={alignment}
+                exclusive
+                onChange={handleChange}
+                aria-label="Platform"
+                size="small"
+            >
+                <ToggleButton value="standard">Standard</ToggleButton>
+                <ToggleButton value="datorbyggare">Datorbyggare</ToggleButton>
+            </ToggleButtonGroup>
             <ul id={showNav ? "" : "hide-nav"}>
                 {links.map((link, index) => {
                     if ("header" in link) {
